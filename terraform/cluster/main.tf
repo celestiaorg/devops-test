855d6b83e913637a6f72d75f07d682e7b43b0dcc
Josh Bowen <jskybowen@gmail.com>
2021-12-01 19:41:29 +0200

    Cleanup main.tf

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_providers {
-    /*
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-    */
     digitalocean = {
         source = "digitalocean/digitalocean"
         version = "~> 2.0"
@@ -18,23 +12,6 @@ variable "do_token" {}
 provider "digitalocean" {
     token = var.do_token
 }
-/*
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
-*/
 
 resource "digitalocean_kubernetes_cluster" "foo" {
   name   = "foo"
@@ -44,7 +21,9 @@ resource "digitalocean_kubernetes_cluster" "foo" {
 
   node_pool {
     name       = "worker-pool"
+    # List possible slugs with `doctl compute size list`
     size       = "s-2vcpu-2gb"
+    # Minimum node_count is 2
     node_count = 2
   }
 }
