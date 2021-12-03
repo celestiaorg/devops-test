7c62996da80f7459e2f525005824c89024104eab
Josh Bowen <jskybowen@gmail.com>
2021-12-03 02:36:07 +0200

    fix sed

--- celestia-app/seed/config.sh
+++ celestia-app/seed/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set proper defaults and change ports
-sed -i 's/"tcp://127.0.0.1:26657"/"tcp://0.0.0.0:26657"/g' $APP/$CONFIG/config/config.toml
+sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $APP/$CONFIG/config/config.toml
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $APP/$CONFIG/config/config.toml
 sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $APP/$CONFIG/config/config.toml
 sed -i 's/index_all_keys = false/index_all_keys = true/g' $APP/$CONFIG/config/config.toml
