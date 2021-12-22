099c401091f0d0cb68287f58e414d8a81282ee0c
Josh Bowen <jskybowen@gmail.com>
2021-12-21 21:59:48 -0700

    Replace config.toml file mounts with `--p2p.mutual` flag in docker-compose (#51)
    * replace mutual peers volume mounts in full nodes with flag
    
    * remove unneeded config.toml files from light nodes
    
    * update changelog

--- devnet/celestia-node/full/full0/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --network devnet_localnet \
-    -v ${PWD}/nodekey0:/celestia-full/keys/OAZHALLLMV4Q:ro \
-    -v ${PWD}/config-full0.toml:/celestia-full/config.toml:ro \
-    jbowen93/celestia-node:devnet full -r /celestia-full start \
-    --core.remote tcp://192.167.10.2:26657 \
-    --headers.trusted-hash A9272DAD1BEBEF104EBDBF071E39789E21C0650AF5B76A7062213B772B6B925E
